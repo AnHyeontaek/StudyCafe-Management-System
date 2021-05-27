40c6bf76e8dbb668db909266c4157fdf855aa2b8
tak <tso5245@naver.com>
2021-05-28 05:00:18 +0900

    프로젝트 폴더 이름 변경 및 파일 커밋

---
+++ Project/src/main/java/com/mycompany/login/MediatorLog.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.login;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author tso52
+ */
+public class MediatorLog  {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MediatorLog log = new MediatorLog();
+        int tema = log.start();
+        log.select(tema);
+
+    }
+
+    public int start() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("로그인을 진행합니다. 1:회원  2:회원 가입 3:관리자"); //정수입력으로 사용자가 원하는 입력 진행
+            int i = sc.nextInt();
+
+            return i;
+        } catch (InputMismatchException e) { //정수가 아닌 입력이 올 경우 예외처리
+            return 0;
+        }
+    }
+    
+    public void select(int a){ //사용자의 입력에 따른 기능을 switch 문으로 실행
+                switch (a) {
+            case 1: 
+                client cl = new client(); //회원 로그인 객체 생성
+                cl.clientplay(cl.inputid(),cl.inputpw()); //회원 아이디와 비밀번호를 입력받아 로그인 진행
+                break;
+            case 2:
+                JoinMember jm = new JoinMember(); //회원가입 객체 생성
+                jm.join(jm.inputid(), jm.inputpw(), jm.inputName(), jm.inputBirth());  //사용자에게 회원가입 정보를 입력받아 매개변수로 넘김
+                break;
+            case 3:
+                 admin ad = new admin(); //관리자 로그인 객체 생성
+                 ad.adminplay(ad.inputid(), ad.inputpw()); //관리자 아이디와 비밀번호를 입력받아 관리자 로그인 실행
+                break;
+            default:
+                System.out.println("입력 오류, 선택사항을 정수로 입력바람. 다시 실행 해주세요.");
+                break;
+        }
+    
+    }
+
+}
